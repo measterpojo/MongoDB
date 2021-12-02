8883afecb21db508d77167445d360faac3fee8f2
Peter Perez <peterperez847@yahoo.com>
2021-12-02 11:51:28 -0600

    first commit

---
+++ src/infrastructure/state.py
@@ -0,0 +1,11 @@
+active_account = None
+
+
+def reload_account():
+    global active_account
+    if not active_account:
+        return
+    
+    # TODO: pull owner account from the database.
+    pass
+    
